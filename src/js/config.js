f6121fb0d1107268f3c62da5ee548ba1cf5c1815
mikias worku <94218813+michaWorku@users.noreply.github.com>
2022-03-23 01:32:09 +0300

    key added

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'd9b32785-448e-4a24-b0d4-2b919fb1846e';
 export const MODAL_CLOSE_SEC = 2.5;
